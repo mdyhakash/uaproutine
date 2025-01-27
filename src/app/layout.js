ab51ed896d536ee49bfd69d5bdd3c0128e62cea7
mdyhakash <mdyhakash@gmail.com>
2025-01-27 23:05:21 +0600

    Favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <DashboardProvider>
           {children}
